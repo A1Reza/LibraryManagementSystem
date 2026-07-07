fef7d8ae75327c72d4afa1a96e6d41b165efe1a5
Reza <reza.asadipour1@gmail.com>
2026-07-07 09:44:53 +0330

    feat: implement find book by id

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -31,4 +31,33 @@ public class BookRepository {
             e.printStackTrace();
         }
     }
+
+    public Book findBookById(int id) throws BookNotFoundException {
+
+        String sql = "SELECT * FROM book WHERE id = ?";
+
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+
+                return new Book(
+                        resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getString("author"),
+                        resultSet.getDouble("price"),
+                        resultSet.getInt("stock")
+                );
+            }
+
+            throw new BookNotFoundException("Book with id " + id + " not found.");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
